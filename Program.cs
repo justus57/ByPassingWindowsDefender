01969fd9ef4bdca8ece0f96ea282548c62d44e35
Joseph K Justus <justuskasyoki57@gmail.com>
2022-05-18 10:45:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByPassingWindowsDefender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buf = new byte[0] { };
+
+            byte[] encoded = new byte[buf.Length];
+
+            for (int i = 0; i < buf.Length; i++)
+            {
+                encoded[i] = (byte)(((uint)buf[i] ^ 0xAA) & 0xFF);
+            }
+            StringBuilder hex = new StringBuilder(encoded.Length * 2);
+            foreach (byte b in encoded)
+            {
+                hex.AppendFormat("0x{0:x2}, ", b);
+            }
+            Console.WriteLine("The payload is: " + hex.ToString());
+        }
+    }
+}
