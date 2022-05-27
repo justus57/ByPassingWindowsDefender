cea67a75749c679e9f5005ee5c749ea7aeaae8aa
Joseph K Justus <justuskasyoki57@gmail.com>
2022-05-27 15:42:41 +0300

    27th May

--- Program.cs
+++ Program.cs
@@ -16,13 +16,19 @@ namespace ByPassingWindowsDefender
 
             for (int i = 0; i < buf.Length; i++)
             {
+
                 encoded[i] = (byte)(((uint)buf[i] ^ 0xAA) & 0xFF);
+
             }
             StringBuilder hex = new StringBuilder(encoded.Length * 2);
+
             foreach (byte b in encoded)
             {
+
                 hex.AppendFormat("0x{0:x2}, ", b);
+
             }
+
             Console.WriteLine("The payload is: " + hex.ToString());
         }
     }
